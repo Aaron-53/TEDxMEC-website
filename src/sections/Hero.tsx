783a71c5008c3a44dcdf40496815cad16115c878
shahal <workshahal@gmail.com>
2026-02-10 23:07:29 +0530

    Added Landing Text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,40 @@
 import PlasmaHero from "@/components/plasma";
-
+import { useState } from "react";
 export default function Hero() {
+const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+const handleNavClick = (e: React.MouseEvent<HTMLAnchorElement>, href: string) => {
+    e.preventDefault();
+    setIsMobileMenuOpen(false);
+    
+    const target = document.querySelector(href);
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <section
       className="min-h-screen relative h-screen w-screen z-10 flex items-center bg-transparent"
       id="hero"
     >
       {/* hero content here */}
-      <PlasmaHero />
+<div className="text absolute  w-screen h-screen z-10 flex flex-col justify-center items-center">
+<span className="text-6xl font-light"><span className="font-bold text-red-600">TED<sup className="text-3xl">X</sup></span>MEC</span>
+<span className="font-bold">Ideas <span className="font-light">Worth</span> Spreading.</span>
+<div className="hidden lg:flex items-center gap-8 mt-10">
+            <a
+              href="#tickets"
+              onClick={(e) => handleNavClick(e, "#tickets")}
+              className="px-4 py-2 bg-tedx-red text-white font-bold text-xs uppercase tracking-wider rounded-md hover:shadow-glow transition-all"
+            >
+              <span className="font-light">Get</span> Tickets
+            </a>
+          </div>
+
+</div>
+<div>
+<PlasmaHero />
+</div>
+      
     </section>
   );
 }
